47aea3e90900cd3b7fac71b94c613df87f04b241
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-03 12:26:48 +0300

    Ses İle Çağırmak

---
+++ Ses İle Çağırmak/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+namespace Ses_İle_Çağırmak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine();
+
+        SpeechSynthesizer speechSymn = new SpeechSynthesizer();
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+             pictureBox1.Visible = false;
+            sestanıma_ayarları();
+           
+            recoEngine.RecognizeAsync();
+        }
+        void sestanıma_ayarları()
+        {
+            string[] ihtimaller = { "Merhaba ", "Hello", "Tolga", "Muzik", "Pop" };
+            Choices seçenekler = new Choices(ihtimaller);
+            Grammar grammer = new Grammar(new GrammarBuilder(seçenekler));
+            recoEngine.LoadGrammar(grammer);
+            recoEngine.SetInputToDefaultAudioDevice();
+            recoEngine.SpeechRecognized += ses_tanıdığında;         
+        
+        }
+
+        void ses_tanıdığında(object sender, SpeechRecognizedEventArgs e)
+        {
+            pictureBox1.Visible = true;
+
+            MessageBox.Show(e.Result.Text);
+        }
+    }
+}
+//Baslaman once bilgisayarimizi ingilizce olarak baslatiyoruz 
